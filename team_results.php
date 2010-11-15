b137533ef3848f4b9dea836bab984473e2d0254e
Ulfounet <Ulfounet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-15 16:02:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/team-results-widget-displaying-scores-for-teams/tags/1.1@311702 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- team_results.php
+++ team_results.php
@@ -4,7 +4,7 @@ Plugin Name: Team results
 Plugin URI: http://didoune.fr/blog/team-results/
 Description: Display results of teams playing sport or e-sport
 Author: Thomas Lamey
-Version: 1.0
+Version: 1.1
 Author URI: http://didoune.fr
 */
 
